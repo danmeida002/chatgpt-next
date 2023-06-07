b3c13cc9cdde70f3c16a13e19e7edaa1324fef93
danmeida002 <135785505+danmeida002@users.noreply.github.com>
2023-06-07 09:59:28 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ import { SettingsProvider } from '@/context/SettingsContext';
 import { isWeChat as utilIsWeChat } from '@/utils/device';
 
 export const metadata: Metadata = {
-  title: 'ChatGPT Next',
+  title: '蛋美达AI智慧助手',
   description: '微信风格的 ChatGPT，基于 Next.js 构建，私有化部署的最佳选择！',
   icons: { icon: '/chatgpt-icon-green.png', apple: '/chatgpt-icon-green.png' },
   viewport: { width: 'device-width', initialScale: 1, viewportFit: 'cover' },
